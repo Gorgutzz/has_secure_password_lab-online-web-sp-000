35f5d00400d2688523482de2f942c8d24abca414
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-08 20:53:09 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create, :show]
+  get 'users/new'
+  post 'users/create'
+
+  post 'sessions/create'
 
 end

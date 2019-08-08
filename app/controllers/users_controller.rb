5b2c818f8513619440a40211c2e8aea0370d1819
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-08 20:45:55 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
 
 
 end

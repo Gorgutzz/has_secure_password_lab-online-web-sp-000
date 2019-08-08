144f416816b1bfe5426e4ce0b6867fd95192cb9c
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-08 20:48:11 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,5 +18,10 @@ class UsersController < ApplicationController
     @user = User.find(session[:user_id])
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 
 end
